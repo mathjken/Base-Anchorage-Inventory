8b12791c8e7e35863f53f5cbb6f165311a9e0322
mathjken <ogujohnkennedy@gmail.com>
2023-05-20 00:31:15 +0100

    views and urls

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1 style="color: red">This is the index page</h>')
+
+
+def staff(request):
+    return HttpResponse('This is the staff page')
