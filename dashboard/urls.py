9844c66b762a3ae359427cca4e54908e3fc5d964
mathjken <ogujohnkennedy@gmail.com>
2023-06-01 05:00:08 +0100

    templates

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('staff/', views.staff, name='staff')
+    path('', views.index, name='dasboard-index'),
+    path('staff/', views.staff, name='dashboard-staff'),
+    path('product/', views.product, name='dashboard-staff'),
+    path('order/', views.order, name='dashboard-staff'),
 ]
--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -122,3 +122,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = (BASE_DIR/"assert/")
