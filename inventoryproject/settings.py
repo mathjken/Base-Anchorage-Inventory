f80d53dd1e4b79f783c0bc1ba53c708addc6adfb
mathjken <ogujohnkennedy@gmail.com>
2023-06-23 02:26:37 +0100

    postgresql implemented

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -78,10 +78,21 @@ WSGI_APPLICATION = "inventoryproject.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+}"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "inventoryappdb",
+        "USER": "postgres",
+        "PASSWORD": "fileee",
+        "PORT": "5433",
+        "HOST": "localhost",
     }
 }
 
